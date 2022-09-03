b61f62b59dd29e947de7aa86585cb9c075e5af00
tomfutago <tomfutago@gmail.com>
2022-09-03 18:52:30 +0100

    added v1 pull for topics

--- discourse/discourse/main.py
+++ discourse/discourse/main.py
@@ -13,14 +13,17 @@ discourse_api_key = os.getenv("DISCOURSE_API_KEY")
 discourse_api_username = os.getenv("DISCOURSE_API_USERNAME")
 db_string = os.getenv('DB_STRING')
 
+# create db engine
+db = create_engine(db_string)
+
+
+########################################################
+# Categories
 api_query = '{}/categories.json'.format(discourse_url)
 
 # retrieve json results and convert to dataframe
 result = requests.get(api_query).json()
-result_items = result.items()
-result_list = list(result_items)
-result_list_of_dict = result_list[0][1].get('categories')
-df = pd.json_normalize(result_list_of_dict)
+df = pd.json_normalize(result['category_list']['categories'])
 
 # include only necessary columns
 list_of_col_names = ['id', 'name', 'slug']
@@ -28,15 +31,80 @@ df = df.filter(list_of_col_names)
 df['created_at'] = datetime.today()
 
 # check existing records for MAX(id)
-db = create_engine(db_string)
 with db.connect() as conn:
-    result = conn.execute(statement='SELECT MAX(id) AS max_id FROM daodash_ny.categories')
+    result = conn.execute(statement='SELECT MAX(id) AS max_id FROM daodash_ny.discourse_categories')
     for row in result:
         max_id = row.max_id
-        print('max_id: ', max_id)
+    if max_id is None:
+        max_id = 0
+    #print('discourse_categories max_id: ', max_id)
 
 # include only new records (id > max_id)
 df_filtered = df[df['id'].gt(max_id)]
 
 # insert into records to db table
-df_filtered.to_sql(name='categories', schema='daodash_ny', con=db, if_exists='append', index=False)
+df_filtered.to_sql(name='discourse_categories', schema='daodash_ny', con=db, if_exists='append', index=False)
+
+
+########################################################
+# Topics
+for page_n in range(50): # check for more_topics_url instead?
+    api_query = '{}/latest.json?order=created&ascending=true&api_key={}&api_username={}&page={}'.format(
+        discourse_url, discourse_api_key, discourse_api_username, page_n
+    )
+
+    # retrieve json results
+    result = requests.get(api_query).json()
+    #print(result['topic_list']['topics'])
+    df = pd.json_normalize(result['topic_list']['topics'])
+
+    # check if current page contains any topics, exit loop if it doesn't
+    if df.empty is True:
+        break
+
+    # change json column names to match table column names
+    df = df.rename(columns={
+        'views': 'views_count',
+        'pinned_globally': 'is_pinned',
+        'visible': 'is_visible',
+        'closed': 'is_closed',
+        'archived': 'is_archived'
+    }, inplace=False)
+
+    # extract original and most recent poster using list comprehansion
+    #[print(n, x['description'], x['user_id']) for n in range(0, len(df)) for x in df['posters'][n] if 'Original Poster' in x['description']]
+    df['user_id'] = [
+        x['user_id'] 
+            for n in range(0, len(df)) 
+                for x in df['posters'][n] 
+                    if 'Original Poster' in x['description']
+    ]
+    df['last_post_user_id'] = [
+        x['user_id'] 
+            for n in range(0, len(df)) 
+                for x in df['posters'][n] 
+                    if 'Most Recent Poster' in x['description']
+    ]
+
+    # include only necessary columns
+    list_of_col_names = [
+        'id', 'title', 'slug', 'user_id', 'category_id', 'created_at', 'last_post_user_id', 'last_posted_at', 
+        'views_count', 'posts_count', 'reply_count', 'like_count', 'is_pinned', 'is_visible', 'is_closed', 'is_archived'
+    ]
+    df = df.filter(list_of_col_names)
+    #print(df)
+
+    # check existing records for MAX(id)
+    with db.connect() as conn:
+        result = conn.execute(statement='SELECT MAX(id) AS max_id FROM daodash_ny.discourse_topics')
+        for row in result:
+            max_id = row.max_id
+        if max_id is None:
+            max_id = 0
+        #print('discourse_categories max_id: ', max_id)
+
+    # include only new records (id > max_id)
+    df_filtered = df[df['id'].gt(max_id)]
+
+    # insert into records to db table
+    df_filtered.to_sql(name='discourse_topics', schema='daodash_ny', con=db, if_exists='append', index=False)
--- discourse/discourse/sql/_generic/tables.sql
+++ discourse/discourse/sql/_generic/tables.sql
@@ -1,5 +1,7 @@
+/*
 DROP TABLE IF EXISTS daodash_ny.discourse_categories;
 DROP TABLE IF EXISTS daodash_ny.discourse_topics;
+*/
 
 CREATE TABLE IF NOT EXISTS daodash_ny.discourse_categories (
     id integer NOT NULL CONSTRAINT categories_pk PRIMARY KEY,
@@ -11,14 +13,15 @@ CREATE TABLE IF NOT EXISTS daodash_ny.discourse_categories (
 
 CREATE TABLE IF NOT EXISTS daodash_ny.discourse_topics (
     id integer NOT NULL CONSTRAINT topics_pk PRIMARY KEY,
-    title character varying NOT NULL,
-    slug character varying,
-    user_id integer,
-    category_id integer,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL,
-    last_post_user_id integer NOT NULL,
-    last_posted_at timestamp without time zone,
+    title varchar(100) NOT NULL,
+    slug varchar(100) NULL,
+    user_id integer NOT NULL,
+    category_id integer NOT NULL,
+    description text NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NULL,
+    last_post_user_id integer NULL,
+    last_posted_at timestamp NULL,
     views_count integer DEFAULT 0 NOT NULL,
     posts_count integer DEFAULT 0 NOT NULL,
     reply_count integer DEFAULT 0 NOT NULL,
@@ -27,6 +30,6 @@ CREATE TABLE IF NOT EXISTS daodash_ny.discourse_topics (
     is_visible boolean DEFAULT true NOT NULL,
     is_closed boolean DEFAULT false NOT NULL,
     is_archived boolean DEFAULT false NOT NULL,
-    deleted_by_id integer,
-    deleted_at timestamp without time zone
+    deleted_by_user_id integer NULL,
+    deleted_at timestamp NULL
 );
